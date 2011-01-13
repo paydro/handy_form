a5c8ef0001cb629fa0d44d1e9604d12b626508b4
Peter Bui <peter@paydrotalks.com>
2011-01-13 14:56:41 -0800

    Add rake tasks to copy form css files to rails dir

--- lib/handy_form/railtie.rb
+++ lib/handy_form/railtie.rb
@@ -3,11 +3,14 @@ require 'rails'
 
 module HandyForm
   class Railtie < Rails::Railtie
-    initializer "Handy Form Initializer" do
-      puts "EXECUTED!"
+    initializer "handy_form.initializer" do |app|
       ::ActionView::Base.class_eval do
         include HandyForm::Helpers
       end
+    end
+
+    rake_tasks do
+      load "handy_form/tasks.rake"
     end
   end
 end
