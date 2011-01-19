186a42afe6d2b90bbfa2efbd19c718185fc9ce7d
Peter Bui <peter@paydrotalks.com>
2011-01-19 12:14:01 -0800

    Remove Rails' default error handling for inputs

--- lib/handy_form/railtie.rb
+++ lib/handy_form/railtie.rb
@@ -4,9 +4,10 @@ require 'rails'
 module HandyForm
   class Railtie < Rails::Railtie
     initializer "handy_form.initializer" do |app|
-      ::ActionView::Base.class_eval do
+      ActionView::Base.class_eval do
         include HandyForm::Helpers
       end
+      ActionView::Base.field_error_proc = Proc.new { |tag, inst| tag }
     end
 
     rake_tasks do
